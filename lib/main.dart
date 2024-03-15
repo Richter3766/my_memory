0a24ff154971eb481ebc0fc417e91f6416f8ab94
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-03-15 19:37:55 +0900

    푸쉬알림 기능 추가 with FCM

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:my_memory/views/restart_widget.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  FirebaseMessaging.instance.requestPermission(
+    badge: true,
+    alert: true,
+    sound: true,
+  );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? message) {
+    if (message != null) {
+      if (message.notification != null) {
+        // print(message.notification!.title);
+        // print(message.notification!.body);
+      }
+    }
+  });
+  FirebaseMessaging.instance
+      .getInitialMessage()
+      .then((RemoteMessage? message) {
+    if (message != null) {
+      if (message.notification != null) {
+        // print(message.notification!.title);
+        // print(message.notification!.body);
+        // print(message.data["click_action"]);
+      }
+    }
+  });
   runApp(RestartWidget(key: restartWidgetKey, rootWidget: const MyMemory()));
 }
 
 final GlobalKey<RestartWidgetState> restartWidgetKey = GlobalKey<RestartWidgetState>();
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
