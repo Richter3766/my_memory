a4ece63226cfd204df6233197f90845eeb35c963
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-03-17 21:35:59 +0900

    기억 리스트 리팩토링

--- lib/views/pages/home_page/widgets/body/memory_list.dart
+++ lib/views/pages/home_page/widgets/body/memory_list.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../../../../provider/db_state.dart';
 import '../../../../../models/post_item.dart';
-import '../../../../../services/db_service.dart';
 import '../../../../../style/colors.dart';
 import '../../../post_detail_page/post_detail.dart';
 import 'home_list_element.dart';
@@ -11,47 +10,39 @@ import 'package:provider/provider.dart';
 class HomeMemoryList extends StatelessWidget {
 
   const HomeMemoryList({super.key});
-
   @override
   Widget build(BuildContext context) {
     DatabaseState databaseState = Provider.of<DatabaseState>(context);
+    List<PostItem> postItems = databaseState.postItems;
+
     return Expanded(
-      child: FutureBuilder<List<PostItem>>(
-        future: DatabaseHelper.instance.getAllPostItems(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView(
-              children: snapshot.data!.map((postItem) {
-                return InkWell(
-                  onTap: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailPage(postItem: postItem),
-                      ),
-                    );
-                    databaseState.refresh();
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: listColor,
-                      border: Border.all(color: listColor, width: 2.0),
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 5.0),
-                      child: HomeListTile(postItem: postItem),
-                    ),
-                  ),
-                );
-              }).toList(),
-            );
-          } else if (snapshot.hasError) {
-            return const Center(child: Text('오류가 발생했습니다.'));
-          } else {
-            return const Center(child: Text('기억을 추가해주세요'));
-          }
+      child: ListView.builder(
+        itemCount: postItems.length,
+        itemBuilder: (context, index) {
+          PostItem postItem = postItems[index];
+          return InkWell(
+            onTap: () async {
+              await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailPage(postItem: postItem),
+                ),
+              );
+              databaseState.refresh();
+            },
+            child: Container(
+              margin: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: listColor,
+                border: Border.all(color: listColor, width: 2.0),
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: HomeListTile(postItem: postItem),
+              ),
+            ),
+          );
         },
       ),
     );
